4bd46ed13d302287d583d27d91043135f8abf2a3
sunkarisanthosh <77742585+sunkarisanthosh@users.noreply.github.com>
2022-07-04 16:14:06 +0100

    Update sampledag.py

--- dags/sampledag.py
+++ dags/sampledag.py
@@ -25,10 +25,3 @@ with DAG(
     schedule_interval=None,
     tags=["dag test demo"],
 )  as dag:
-
-    t1= BashOperator(
-        task_id='echo',
-        bash_command= 'echo test',
-        dag=dag,
-        depends_on_past=False,
-        priority_weight=2**31-1)
