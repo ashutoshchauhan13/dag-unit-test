33b6c6683e8852b938cd0fe64a0903be042d580f
sunkarisanthosh <77742585+sunkarisanthosh@users.noreply.github.com>
2022-07-04 13:57:13 +0100

    Update sampledag.py

--- dags/sampledag.py
+++ dags/sampledag.py
@@ -13,7 +13,7 @@ DAG_ID = os.path.basename(__file__).replace(".py", "")
 S3_BUCKET = Variable.get("data_lake_bucket")
 
 DEFAULT_ARGS = {
-    "owner": "garystafford",
+    "owner": "db-volt",
     "depends_on_past": False,
     "retries": 0,
     "email_on_failure": False,
@@ -33,31 +33,4 @@ with DAG(
 
     end = DummyOperator(task_id="end")
 
-    delete_demo_s3_objects = BashOperator(
-        task_id="delete_demo_s3_objects",
-        bash_command=f'aws s3 rm "s3://{S3_BUCKET}/tickit/" --recursive',
-    )
 
-    list_demo_s3_objects = BashOperator(
-        task_id="list_demo_s3_objects",
-        bash_command=f"aws s3api list-objects-v2 --bucket {S3_BUCKET} --prefix tickit/",
-    )
-
-    delete_demo_catalog = BashOperator(
-        task_id="delete_demo_catalog",
-        bash_command='aws glue delete-database --name tickit_demo || echo "Database tickit_demo not found."',
-    )
-
-    create_demo_catalog = BashOperator(
-        task_id="create_demo_catalog",
-        bash_command="""aws glue create-database --database-input \
-            '{"Name": "tickit_demo", "Description": "Datasets from AWS E-commerce TICKIT relational database"}'""",
-    )
-
-chain(
-    begin,
-    (delete_demo_s3_objects, delete_demo_catalog),
-    (list_demo_s3_objects, create_demo_catalog),
-    end,
-)
-# 
